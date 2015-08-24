676b87d688f7784b4e18767d143d7bd9eb475081
Mariano Koldobsky <mariano.koldobsky@mercadolibre.com>
2015-08-24 12:27:55 -0300

    ListView working on hardcoded data

--- app/src/main/java/com/example/mkoldobsky/sunshine/MainActivity.java
+++ app/src/main/java/com/example/mkoldobsky/sunshine/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.mkoldobsky.sunshine;
 
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
     }
 
--- app/src/main/java/com/example/mkoldobsky/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/mkoldobsky/sunshine/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +21,28 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+        final ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        String[] forecasts = getForecasts();
+        final ArrayList<String> list = new ArrayList<>();
+        for(int i = 0;  i< forecasts.length; ++i){
+            list.add(forecasts[i]);
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(inflater.getContext(), android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
+
+
+    private String[] getForecasts() {
+        String[] result = new String[]{"Today - sunny - 10ª/2ª", "Tomorrow - cloudy - 12ª/4ª", "Wendesday - cluody - 15ª/6ª", "Thurdsday - raining - 17ª/10", "Friday - cloudy - 20ª-14ª",
+                "Saturday - cloudy - 20ª/14ª", "Sunday - sunny - 25ª/20ª"};
+        return result;
+
+    }
+
+
+
 }
